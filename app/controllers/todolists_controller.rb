4d62bc964729431c3da10a2ec7c5e3a8a28de043
EC2 Default User <ec2-user@ip-172-31-42-211.ap-northeast-1.compute.internal>
2022-09-12 03:19:36 +0000

    Chapter 6 編集機能を作ろう

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -18,6 +18,19 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+
+    # ---- ここからコードを書きましょう ---- #
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+    # ---- ここまでのあいだにコードを書きましょう ---- #
+
+  end
+
 
   private
   # ストロングパラメータ
